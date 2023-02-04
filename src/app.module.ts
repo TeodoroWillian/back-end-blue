e67d56ca0c32ec21cd6ff9d05c8482d6d81b53ef
Willian Teodoro <totawillian@gmail.com>
2023-02-04 09:53:35 -0400

    feat(prisma): module & service Created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module'
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GameModule, UserModule],
+  imports: [GameModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
